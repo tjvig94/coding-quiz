95eabce9bc55b22cfba673dd49044e1def003d77
Tim Vigneau <tjvig94@gmail.com>
2021-01-12 19:58:11 -0500

    Quiz area fills with question and answer options

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
-const questions = [
-    "What does DOM stand for?",
-    "What does API stand for?",
-    "Which of the following is a value for the CSS property, position?",
-    "Which of the following is the propery syntax for a ternary conditional?"
-];
-
-const startButton = document.querySelector(".startBtn");
-const timer = document.querySelector(".timer");
-// var score = 
-
-function countdown() {
-    let secondsLeft = 60;
-    count = setInterval(function() {
-        secondsLeft--;
-        timer.textContent = secondsLeft;
-        localStorage.setItem("score", secondsLeft);
-        if (secondsLeft === 55) {
-            clearInterval(count);
-        };
-    }, 1000);
+var question1 =  
+    {question: "What does DOM stand for?", correct: "Document Object Model", wrong1: "Donut Oreo Muffin", wrong2: "Document Order Master", wrong3: "Delivered Object Model"};
 
 
+$(document).ready(function() {
+    
+    function game() {
+        // Put question in questionArea
+        $(".questionArea").text(question1.question);
 
-}
+        // Put answer options in the answers list
+        $(".answer1").text(question1.wrong1);
+        $(".answer2").text(question1.correct);
+        $(".answer3").text(question1.wrong3);
+        $(".answer4").text(question1.wrong2);       
 
+        // Timer
+        let secondsLeft = 60;
+        count = setInterval(function() { 
+            secondsLeft--;
+            $(".timer").text(secondsLeft);
+            localStorage.setItem("score", JSON.stringify(secondsLeft));
+            if (secondsLeft === 55) {
+                clearInterval(count);
+            };
+        }, 1000);
+        
+    }
 
+    
+    
+    $(".startBtn").on("click", game);
 
+})
 
-startButton.addEventListener("click", countdown);
 
 
 

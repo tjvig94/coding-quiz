27ba97bbd97e794273a1b1b67f6e8fb35db1d97e
Tim Vigneau <tjvig94@gmail.com>
2021-01-15 15:17:55 -0500

    Checks for right/wrong answers

--- js/script.js
+++ js/script.js
@@ -1,25 +1,57 @@
 $(document).ready(function() {
 
     const questions = [ 
-        {question: "What does DOM stand for?", correct: "Document Object Model", wrong1: "Donut Oreo Muffin", wrong2: "Document Order Master", wrong3: "Delivered Object Model"},
-        {question: "What does API stand for?", correct: "Application Programming Interface", wrong1: "Application Pilot Interchange", wrong2: "Array Parsing Integer", wrong3: "Apple Pie Ingestor"},
-        {question: "Which of the following is part of the MERN stack?", correct: "React.js", wrong1: "Nord.js", wrong2: "MangoDB", wrong3: "Eggscript"},
-        {question: "What charset is should be used in your HTML files?", correct: "UTF-8", wrong1: "English", wrong2: "ABC123", wrong3: "charcoal"},
-        {question: "What do you think?", correct: "Yes", wrong1: "No", wrong2: "Maybe", wrong3: "Eh"}    
+        {
+            question: "What does DOM stand for?",
+            a: "Document Object Model", 
+            b: "Donut Oreo Muffin", 
+            c: "Document Order Master", 
+            d: "Delivered Object Model",
+            correctAnswer: "a"
+
+        },
+        {
+            question: "What does API stand for?", 
+            a: "Application Pilot Interchange", 
+            b: "Array Parsing Integer", 
+            c: "Application Programming Interface", 
+            d: "Apple Pie Ingestor",
+            correctAnswer: "c"
+        },
+        {
+            question: "Which of the following is part of the MERN stack?", 
+            a: "Nord.js", 
+            b: "MangoDB", 
+            c: "Eggscript", 
+            d: "React.js",
+            correctAnswer: "d"
+        },
+        {
+            question: "What charset is should be used in your HTML files?", 
+            a: "UTF-8", 
+            b: "English", 
+            c: "ABC123", 
+            d: "charcoal",
+            correctAnswer: "a"
+        }    
             ];
+
+        
 
     let secondsLeft = 60;
     
     let i = 0;
     function populateQuiz(i) {
         if (i >= questions.length || secondsLeft <= 0) {
-            alert("Game over!!!! yikes....");
+            $(".questionBox").hide();
+            $(".highScoreBox").show();
+            clearInterval(count);
         } else {
             $(".questionArea").text(questions[i].question);
-            $(".answer1").text(questions[i].correct).attr("data-correct","true");
-            $(".answer2").text(questions[i].wrong1).attr("data-correct","false");
-            $(".answer3").text(questions[i].wrong2).attr("data-correct","false");
-            $(".answer4").text(questions[i].wrong3).attr("data-correct","false");
+            $(".answer1").text(questions[i].a);
+            $(".answer2").text(questions[i].b);
+            $(".answer3").text(questions[i].c);
+            $(".answer4").text(questions[i].d);
         };           
                                                  
     };
@@ -45,22 +77,27 @@ $(document).ready(function() {
 
     // Repopulate fields with new question and answers
     $(".answer").on("click", function() {
+    // Check to see if answer is correct or not
+        if (this.dataset.correct !== questions[i].correctAnswer) {
+            secondsLeft = secondsLeft-15;
+        };        
         i++;
         populateQuiz(i);
-        // Check to see if answer is correct or not
-        if (this.dataset.correct === "false") {
-            console.log(this.dataset.correct);
-            secondsLeft = secondsLeft-15;
-        };
     });
-    
+
     $(".startBtn").on("click", game);
 
+    $(".submitName").on("click", function() {
+        let playerName = $(".nameInput").value;
+        localStorage['name'] = playerName;
+    });
 
 
-    
 
 });
+    
+
+
 
 
 
